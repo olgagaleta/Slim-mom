8c30cf7fe086df5bb207d0b970cfeebac1276f85
VitaliyLev <111646762+VitaliyLev@users.noreply.github.com>
2023-01-16 16:17:49 +0200

    add base background

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,15 @@
 import { GlobalStyle } from '../style/GlobalStyle';
+import { PageContainer } from './PageContainer/PageContainer';
 
 export const App = () => {
   return (
     <>
       {/* ці стилі застовуються до всієї апки та скинуть наші налаштування */}
       <GlobalStyle />
+
+      <PageContainer>
+        {/* тут будем рендерити всю розмітку в головному контейнері */}
+      </PageContainer>
     </>
   );
 };
