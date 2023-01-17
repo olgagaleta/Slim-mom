6676f10324ecdfd9247bc8573268a152fda5d922
VitaliyLev <111646762+VitaliyLev@users.noreply.github.com>
2023-01-17 12:55:06 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,5 @@
-
 import { Routes, Route } from 'react-router-dom';
 import { lazy } from 'react';
-
-import { register } from 'redux/auth/auth-options';
 
 import { GlobalStyle } from '../style/GlobalStyle';
 import { PageContainer } from './PageContainer/PageContainer';
@@ -10,7 +7,6 @@ import { PageContainer } from './PageContainer/PageContainer';
 const Calculator = lazy(() => import('../pages/CalculatorPage'));
 
 export const App = () => {
-  console.log(register());
   return (
     <>
       {/* ці стилі застовуються до всієї апки та скинуть наші налаштування */}
@@ -19,12 +15,8 @@ export const App = () => {
       <PageContainer>
         {/* тут будем рендерити всю розмітку в головному контейнері */}
         <Routes>
-          <Route
-            path='calculator'
-            element={<Calculator/>}
-          />
+          <Route path="calculator" element={<Calculator />} />
         </Routes>
-        
       </PageContainer>
     </>
   );
