550692486ecbabe74bd9c2b36b3b80e13cedda8a
VitaliyLev <111646762+VitaliyLev@users.noreply.github.com>
2023-01-19 21:46:10 +0200

    add fix

--- src/services/theme/theme.js
+++ src/services/theme/theme.js
@@ -1,33 +1,29 @@
 export const momsTheme = {
-  sideBarColor: '#F0F1F3',
   color: 'white',
-  modalColor: 'white',
+  sideBarColor: '#F0F1F3',
   modalTextColor: '#264061',
-  modalBorderColor: '#fc842d',
   calendarColor: '#f0f0f0',
   gradient: true,
-
-  colorTextPrimary: 'rgba(0, 0, 0, 0.87)',
-  colorTextSecondary: 'rgba(0, 0, 0, 0.6)',
-  colorBackgroundDefault: '#fff',
-
-  colorTextWhite: '#121212',
   
+  colorTextPrimary: 'rgba(0, 0, 0, 0.87)',
+  colorTextWhite: '#121212',
+  // modalColor: 'white',
+  // modalBorderColor: '#fc842d',
+  // colorTextSecondary: 'rgba(0, 0, 0, 0.6)',
+  // colorBackgroundDefault: '#fff',
 };
 
 export const darkTheme = {
-  modalColor: '#165181',
-  modalTextColor: 'white',
-  modalBorderColor: '#165181',
-  calendarColor: '#f0f0f0',
-  gradient: false,
-
   color:'#424242',
   sideBarColor: 'rgba(0, 0, 0, 0.1)',
-
+  modalTextColor: 'white',
+  calendarColor: '#f0f0f0',
+  gradient: false,
   colorTextPrimary: '#fff',
-  colorTextSecondary: 'rgba(255, 255, 255, 0.7)',
-  colorBackgroundDefault: '#121212',
-
   colorTextWhite: '#fff',
+  
+  // colorTextSecondary: 'rgba(255, 255, 255, 0.7)',
+  // colorBackgroundDefault: '#121212',
+  // modalBorderColor: '#165181',
+  // modalColor: '#165181',
 };
