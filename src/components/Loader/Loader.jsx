7a4213c5f98e5b263b02a8c4187da23851b17aa3
Dmytro <111579384+Dmytro1117@users.noreply.github.com>
2023-01-19 11:00:38 +0200

    loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,32 +1,54 @@
-import {
-  // Arrow,
-  Container,
-  // Spinner, SpinnerContainer
-} from './Loader.styled';
-// import Scales from 'helpers/images/loader.png';
-// import SpinnerBase from 'helpers/images/loader-mini.png';
-// import { Hearts } from 'react-loader-spinner';
+// import {
+//   // Arrow,
+//   Container,
+//   // Spinner, SpinnerContainer
+// } from './Loader.styled';
+// // import Scales from 'helpers/images/loader.png';
+// // import SpinnerBase from 'helpers/images/loader-mini.png';
+// // import { Hearts } from 'react-loader-spinner';
 
-export default function Loader() {
+// export default function Loader() {
+//   return (
+//     <Container>
+//       {/* <SpinnerContainer>
+//         <img src={Scales} alt="scales" />
+//       </SpinnerContainer>
+//       <Spinner>
+//         <Arrow src={SpinnerBase} alt="spinner" />
+//       </Spinner> */}
+//       {/* <Hearts
+//         height="80"
+//         width="80"
+//         color="#fc842d"
+//         ariaLabel="hearts-loading"
+//         wrapperStyle={{}}
+//         wrapperClass=""
+//         visible={true}
+//         // className="true"
+//         // className="true" or className={value.toString()}.
+//       /> */}
+//     </Container>
+//   );
+// }
+
+import { Triangle } from 'react-loader-spinner';
+import { Container, SpinnerContainer } from './Loader.styled';
+
+export const Loader = () => {
   return (
     <Container>
-      {/* <SpinnerContainer>
-        <img src={Scales} alt="scales" />
-      </SpinnerContainer>
-      <Spinner>
-        <Arrow src={SpinnerBase} alt="spinner" />
-      </Spinner> */}
-      {/* <Hearts
+      <SpinnerContainer>
+      <Triangle
         height="80"
         width="80"
-        color="#fc842d"
-        ariaLabel="hearts-loading"
+        color="green"
+        ariaLabel="triangle-loading"
         wrapperStyle={{}}
-        wrapperClass=""
+        wrapperClassName=""
         visible={true}
-        // className="true"
-        // className="true" or className={value.toString()}.
-      /> */}
-    </Container>
+          />
+          </SpinnerContainer>
+      </Container>
   );
-}
+};
+export default Loader;
