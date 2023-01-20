0bc5704d0f6dd59bb15d2e0a6f902d6dec664fa4
Dmytro <111579384+Dmytro1117@users.noreply.github.com>
2023-01-20 22:16:09 +0200

    favico, calendar

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -1,5 +1,5 @@
 import CalendarButton from './CalendarButton/CalendarButton';
-import { DateContainer, DateString, MyCalendar } from './DatePicker.styled';
+import { DateContainer, DateString, MyCalendar, Overlay } from './DatePicker.styled';
 import 'react-calendar/dist/Calendar.css';
 import { useState } from 'react';
 import { useEffect } from 'react';
@@ -28,33 +28,118 @@ export default function DatePicker({ date, setDate }) {
     return () => {
       window.removeEventListener('keydown', handleCloseCalendar);
     };
+    
   }, []);
-
+ 
   const handleCalendar = async newDate => {
     const rawDate = new Date(newDate);
     setDate(rawDate);
     setShowCalendar(false);
     setSearchParams({ date: rawDate.toLocaleDateString('en-CA') });
   };
-
+  const handleBackdropClick = event => {
+    if (event.currentTarget === event.target) setShowCalendar(false);
+  };
+ 
   return (
+    
     <DateContainer>
+     
       <DateString state={state}>{normalizedDate}</DateString>
       <CalendarButton
         showCalendar={showCalendar}
         onClick={handleToogleCalendar}
       />
+       
       {showCalendar && (
+        <Overlay onClick={handleBackdropClick}>
         <MyCalendar
-          maxDate={new Date()}
           onClickDay={handleCalendar}
           value={date}
         />
-      )}
+       </Overlay>
+      )} 
+      
     </DateContainer>
+    
   );
 }
 DatePicker.propTypes = {
   date: PropTypes.object,
   setDate: PropTypes.func,
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import CalendarButton from './CalendarButton/CalendarButton';
+// import { DateContainer, DateString, MyCalendar } from './DatePicker.styled';
+// import 'react-calendar/dist/Calendar.css';
+// import { useState } from 'react';
+// import { useEffect } from 'react';
+// import { useOutletContext, useSearchParams } from 'react-router-dom';
+// import PropTypes from 'prop-types';
+
+// export default function DatePicker({ date, setDate }) {
+//   const [showCalendar, setShowCalendar] = useState(false);
+//   const [searchParams, setSearchParams] = useSearchParams();
+//   const initCalendarDay = new Date(searchParams.get('date') || date);
+//   const { state } = useOutletContext();
+
+//   const normalizedDate = initCalendarDay
+//     .toLocaleDateString('en-GB')
+//     .replaceAll('/', '.');
+
+//   const handleToogleCalendar = () => {
+//     setShowCalendar(!showCalendar);
+//   };
+//   const handleCloseCalendar = e => {
+//     if (e.key === 'Escape') setShowCalendar(false);
+//   };
+
+//   useEffect(() => {
+//     window.addEventListener('keydown', handleCloseCalendar);
+//     return () => {
+//       window.removeEventListener('keydown', handleCloseCalendar);
+//     };
+//   }, []);
+
+//   const handleCalendar = async newDate => {
+//     const rawDate = new Date(newDate);
+//     setDate(rawDate);
+//     setShowCalendar(false);
+//     setSearchParams({ date: rawDate.toLocaleDateString('en-CA') });
+//   };
+
+//   return (
+//     <DateContainer>
+//       <DateString state={state}>{normalizedDate}</DateString>
+//       <CalendarButton
+//         showCalendar={showCalendar}
+//         onClick={handleToogleCalendar}
+//       />
+//       {showCalendar && (
+//         <MyCalendar
+//           maxDate={new Date()}
+//           onClickDay={handleCalendar}
+//           value={date}
+//         />
+//       )}
+//     </DateContainer>
+//   );
+// }
+// DatePicker.propTypes = {
+//   date: PropTypes.object,
+//   setDate: PropTypes.func,
+// };
